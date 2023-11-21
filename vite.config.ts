afbc8ed28648b214d02b16215b81963df00f7867
madeval <alvarez.fing@gmail.com>
2023-11-21 18:21:08 -0300

    chore: :wrench: test updates config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Toast',
-      fileName: '@madeval/toast',
+      fileName: 'waves',
     },
     rollupOptions: {
       external: ['react', 'react-dom'],

c690d2bbeeb4dc96bc26af6e7c4821e5875c9a0b
madeval <alvarez.fing@gmail.com>
2023-11-22 22:58:16 -0300

    test: setup test environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'testing-library', 'jest-dom'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',

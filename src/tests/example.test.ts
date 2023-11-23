b26488da8899f09cb294822bde0253d009fa4635
madeval <alvarez.fing@gmail.com>
2023-11-22 22:58:32 -0300

    test: :test_tube: add example test

---
+++ src/tests/example.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import {} from '@testing-library/react'
+
+describe('🚨 This is an example! [Remove then]', () => {
+  it('simple test', () => {
+    expect(1).toBe(1)
+  })
+})

7a8247214d0ca77c542316f18cbd0675c5dd3138
madeval <alvarez.fing@gmail.com>
2023-12-01 23:04:24 -0300

    refactor: :fire: remove example test file

--- tests/example.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import {} from '@testing-library/react'
-
-describe('🚨 This is an example! [Remove then]', () => {
-  it('simple test', () => {
-    expect(1).toBe(1)
-  })
-})

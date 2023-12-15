e54b6266390ab65ec2eb6addcb2192156eb03736
madeval <alvarez.fing@gmail.com>
2023-12-15 19:19:32 -0300

    fix: :green_heart: prevent inifinite time build

--- packages/aurora/vite.config.ts
+++ packages/aurora/vite.config.ts
@@ -4,12 +4,18 @@ import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react-swc'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
+// set WATCH='true' on dev script, this will prevent infinite time build
+const isWatchMode = process.env.WATCH === 'true'
+const watch = isWatchMode
+  ? {
+      include: ['src/**'],
+    }
+  : null
+
 export default defineConfig({
   plugins: [react(), dts(), cssInjectedByJsPlugin()],
   build: {
-    watch: {
-      include: './src/**',
-    },
+    watch,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Toast',

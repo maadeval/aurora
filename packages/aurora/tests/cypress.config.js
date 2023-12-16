c03ece9a4e59582ca4a86069edc75a812e1c560f
madeval <alvarez.fing@gmail.com>
2023-12-16 19:32:41 -0300

    fix: add new config of cypress

--- packages/aurora/tests/cypress.config.js
+++ packages/aurora/tests/cypress.config.js
@@ -4,8 +4,6 @@ export default defineConfig({
 	video: false,
 	component: {
 		specPattern: "tests/**/*.cy.{js,jsx,ts,tsx}",
-		supportFile: "tests/tests-config/cypress/support/component.ts",
-		indexHtmlFile: "tests/tests-config/cypress/support/component-index.html",
 		devServer: {
 			framework: "react",
 			bundler: "vite",
